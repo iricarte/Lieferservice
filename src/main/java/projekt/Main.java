33cd5815cad18de1c828107db8e4c110912cd221
Birte Kurth <bkurth@tutanota.com>
2023-02-27 16:50:32 +0100

    Birte first commit

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         startWithGUI();
+        //New Commit
         //startWithoutGUI(); //can bew used instead to run a simulation without a gui
     }
 
