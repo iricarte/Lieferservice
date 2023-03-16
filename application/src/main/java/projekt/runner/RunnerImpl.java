fa23a31b72b19fb843472acb43bc0eae11910a51
iricarte <pro-github-ir@zyrog.com>
2023-03-16 03:16:01 +0100

    FOP-109 H10 RunnerImpl RunnerImpl fully implemented. The maxTick for simulations is a magical number, since I don't really know where to get this number from. This should be changed in the future, if there is a better alternative

--- application/src/main/java/projekt/runner/RunnerImpl.java
+++ application/src/main/java/projekt/runner/RunnerImpl.java
@@ -2,16 +2,17 @@ package projekt.runner;
 
 import projekt.delivery.archetype.ProblemArchetype;
 import projekt.delivery.archetype.ProblemGroup;
+import projekt.delivery.rating.RatingCriteria;
 import projekt.delivery.service.DeliveryService;
+import projekt.delivery.simulation.BasicDeliverySimulation;
 import projekt.delivery.simulation.Simulation;
 import projekt.delivery.simulation.SimulationConfig;
 import projekt.runner.handler.ResultHandler;
 import projekt.runner.handler.SimulationFinishedHandler;
 import projekt.runner.handler.SimulationSetupHandler;
 
+import java.util.HashMap;
 import java.util.Map;
-
-import static org.tudalgo.algoutils.student.Student.crash;
 
 public class RunnerImpl implements Runner {
 
@@ -24,14 +25,61 @@ public class RunnerImpl implements Runner {
                     SimulationFinishedHandler simulationFinishedHandler,
                     ResultHandler resultHandler) {
 
-        crash(); // TODO: H10.2 - remove if implemented
+        Map<ProblemArchetype, Simulation> simulations = this.createSimulations(problemGroup,
+                                                                               simulationConfig,
+                                                                               deliveryServiceFactory);
+
+        Map<RatingCriteria, Double> result = this.runSimulations(simulationRuns,
+                                                                 simulationSetupHandler,
+                                                                 simulationFinishedHandler,
+                                                                 simulations);
+
+        resultHandler.accept(result);
     }
 
     @Override
     public Map<ProblemArchetype, Simulation> createSimulations(ProblemGroup problemGroup,
                                                                SimulationConfig simulationConfig,
                                                                DeliveryService.Factory deliveryServiceFactory) {
+        Map<ProblemArchetype, Simulation> retVal = new HashMap<>();
+        for (ProblemArchetype problem : problemGroup.problems()) {
+            BasicDeliverySimulation simulation = new BasicDeliverySimulation(simulationConfig,
+                                                                             problem.raterFactoryMap(),
+                                                                             deliveryServiceFactory.create(problem.vehicleManager()),
+                                                                             problem.orderGeneratorFactory());
+            retVal.put(problem, simulation);
+        }
+        return retVal;
+    }
 
-        return crash(); // TODO: H10.1 - remove if implemented
+    private Map<RatingCriteria, Double> runSimulations(int simulationRuns,
+                                                       SimulationSetupHandler simulationSetupHandler,
+                                                       SimulationFinishedHandler simulationFinishedHandler,
+                                                       Map<ProblemArchetype, Simulation> simulations) {
+        Map<RatingCriteria, Double> criteriaToAverage = new HashMap<>();
+        for (int i = 0; i < simulationRuns; i++) {
+            for (Map.Entry<ProblemArchetype, Simulation> simulationEntry : simulations.entrySet()) {
+                Simulation simulation = simulationEntry.getValue();
+                ProblemArchetype problem = simulationEntry.getKey();
+                simulationSetupHandler.accept(simulation, problem, i);
+                simulation.runSimulation(1000);
+                this.measureCriteria(simulation, RatingCriteria.TRAVEL_DISTANCE, criteriaToAverage);
+                this.measureCriteria(simulation, RatingCriteria.AMOUNT_DELIVERED, criteriaToAverage);
+                this.measureCriteria(simulation, RatingCriteria.IN_TIME, criteriaToAverage);
+                if (simulationFinishedHandler.accept(simulation, problem)) {
+                    return criteriaToAverage;
+                }
+            }
+        }
+        return criteriaToAverage;
+    }
+
+    private void measureCriteria(Simulation simulation,
+                                 RatingCriteria criteria,
+                                 Map<RatingCriteria, Double> criteriaToAverage) {
+        double ratingForCriterion = simulation.getRatingForCriterion(RatingCriteria.TRAVEL_DISTANCE);
+        criteriaToAverage.merge(criteria,
+                                ratingForCriterion,
+                                (existingRationing, newRating) -> (existingRationing + newRating) / 2);
     }
 }
